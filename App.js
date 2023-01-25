5aaf63b89c337f9ce1f167a24fa9279daa6ec447
Abdul Rehman <114314272+Maani5790@users.noreply.github.com>
2023-01-25 17:15:21 +0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,26 @@
+// import { StatusBar } from 'expo-status-bar';
+// import CounterNumber from './src/CounterNumber';
+import ColorGenerator from './src/ColorGenerator';
+import { View } from 'react-native';
+
+const App = ()=> {
+  return (
+    <View>
+      {/* <Text>Open up App.js to start working on your app!</Text> */}
+      {/* <StatusBar style="auto" /> */}
+      {/* <CounterNumber /> */}
+      <ColorGenerator />
+      
+    </View>
+  );
+}
+
+export default App;
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
